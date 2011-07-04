b3bcc1613564aba71118c5d2e0ec1aef7a7ce845
unknown <Diana@.(none)>
2011-07-04 00:04:17 -0400

    initial import of content

---
+++ doulaDescription.php
@@ -0,0 +1,119 @@
+<?php include("includes/doctype_html.php") ?>
+<?php include("includes/head_start.php") ?>
+<?php include("includes/head_end.php") ?>
+<?php include("includes/body_start.php") ?>
+<?php include("includes/globalnav.php") ?>
+
+        <!-- unique -->
+        <div id="content">
+            
+            <?php include("includes/sidebar.php") ?>
+            
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    What is a doula?
+                </h1>
+                
+                <p>
+                    A doula is a woman experienced in childbirth who provides 
+                    continuous physical, emotional and informational support 
+                    to the mother and her partner before, during and just after 
+                    childbirth.  This includes preparation for birth, helping the 
+                    laboring woman with encouragement, comfort measures, relaxation 
+                    techniques, and an objective viewpoint, helping the mother and 
+                    her partner interact successfully with medical providers, and 
+                    support after childbirth such as helping to initiate breastfeeding.  
+                    Doulas believe that pregnancy, birth and breastfeeding are normal, 
+                    natural and healthy processes.   Doulas understand the physiology 
+                    of birth and the emotional needs of a laboring woman and her partner.  
+                    Doulas do not provide medical advice or perform medical procedures 
+                    of any kind.  A doula will not make medical decisions for the mother 
+                    or partner, but will provide them with the necessary and accurate 
+                    information to make an informed decision.  For answers to some 
+                    common misconceptions regarding doulas read Doula Myths vs Realities.
+                </p>
+            </div>
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    Benefits of Having a Doula
+                </h1>
+                
+                <p>
+                    <ul>
+						<li>
+							50% Reduction in Cesarean Rate
+						</li>
+						<li>
+							25% Reduction in length of labor
+						</li>
+						<li>
+							40% Reduction in the need for labor stimulating drugs
+						</li>
+						<li>
+							60% Reduction in request for  epidural pain relief
+						</li>
+                    </ul>
+                    *Klaus, M.; Klaus, P.; Kennell, J. Mothering the Mother 1993
+                </p>
+            </div>
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    Who Needs a Doula?
+                </h1>
+                
+                <p>
+                    Every pregnant woman will benefit from having a doula present 
+                    during her labor and delivery.  A doula "mother's  the mother" 
+                    in a way no other care provider can.  The doula enhances the 
+                    supporting efforts of the mother's partner, which makes the birth 
+                    experience more meaningful for everyone involved.  There are some 
+                    situations when the presence of a doula becomes even more necessary.
+                </p>
+                
+                <p>    
+                    These include: 
+                    <ul>
+						<li>
+							Couples attempting a VBAC (vaginal birth after cesarean)
+						</li>
+						<li>
+							Women classified as high risk
+						</li>
+						<li>
+							Couples expecting twins or higher order multiples
+						</li>
+						<li>
+							Couples strongly committed to avoiding pain medication or 
+							routine interventions
+						</li>
+						<li>
+							Couples planning a home birth
+						</li>
+						<li>
+							Women laboring without the support of a partner
+						</li>
+						<li>
+							Couples who desire midwifery care but are unable to 
+							find a midwife
+						</li>
+						<li>
+							Couples attempting to overcome a 'bad' birth experience
+						</li>
+                    </ul>
+                </p>
+                <p>
+					To learn more about doulas and access other links about pregnancy, 
+					birth, nutrition and newborns, visit dona.org or doulanetwork.com.
+                </p>
+            </div>           
+        </div>
+        <!-- /unique -->
+
+<?php include("includes/footer.php");?>
+<?php include("includes/body_end.php");?>
+<?php include("includes/html_end.php");?>
+
+
---
+++ midwiferyDescription.php
@@ -0,0 +1,71 @@
+<?php include("includes/doctype_html.php") ?>
+<?php include("includes/head_start.php") ?>
+<?php include("includes/head_end.php") ?>
+<?php include("includes/body_start.php") ?>
+<?php include("includes/globalnav.php") ?>
+
+        <!-- unique -->
+        <div id="content">
+            
+            <?php include("includes/sidebar.php") ?>
+            
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    What is a Direct-Entry Midwife?
+                </h1>
+                
+                <p>
+                    The term midwife generally implies one of three things, 
+                    either a woman has chosen to become a DEM, CPM, or CNM.  
+                    Certified Professional Midwives and Direct Entry Midwives 
+                    practice in the homebirth or sometimes the birth-center 
+                    setting depending on the state in which they practice.  
+                    In Ohio, CPM's and DEM's practice only in the homebirth 
+                    setting while Certified Nurse Midwives practice in a hospital 
+                    setting and maintain a collaborative relationship with doctors.
+                </p>
+                
+                <p>
+					The path through which a midwife has chosen her education 
+					varies from woman to woman and includes an academic program 
+					of study, an apprenticeship or both.  There is no "more correct" 
+					method of education.  Rather, there are different paths that lead 
+					to the same goal of serving birthing couples in the homebirth or 
+					hospital setting.  Interview your midwife as you would any other 
+					care provider.  Ask a lot of questions and make sure you are 
+					comfortable with the answers. 
+                </p>
+                
+                <p>
+					As your DEM I will provide prenatal care for the mother as well 
+					as labor and postpartum care for both the mother and the baby.  
+					I have a special interest in prenatal nutrition, VBAC and waterbirth.  
+					My education is through the Michigan School of Traditional Midwifery.  
+					Additional training includes adult, child and infant CPR with AED,  
+					First Aid, Neonatal Resuscitation, and breastfeeding certifications.    
+					My birth experience has included lots of natural vaginal birth as 
+					well as twins, episiotomy, vacuum, cesarean section, 
+					epidural/narcotic anesthesia, water birth, VBAC, single mothers, 
+					breastfeeding, bottle feeding and special needs infants.
+                </p>
+                
+                <p>
+					Homebirth requires a large amount of personal responsibility for 
+					both your pregnancy and your birth.  While I am present to safeguard 
+					the passage of mother and child through the birth process, you are 
+					ultimately responsible for your birth.  A high level of commitment 
+					to health (mental, physical and spiritual) through birth preparation 
+					and prenatal nutrition is essential.  I cannot stress this enough; 
+					if you do not honestly prepare for your birth during your pregnancy, 
+					no amount of midwifery care will change the outcome.
+                </p>
+            </div>
+        </div>
+        <!-- /unique -->
+
+<?php include("includes/footer.php");?>
+<?php include("includes/body_end.php");?>
+<?php include("includes/html_end.php");?>
+
+
