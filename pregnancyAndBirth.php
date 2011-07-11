212cc1ccbc4e57f245a1c3e2f0ea9b6d7e697840
unknown <Diana@.(none)>
2011-07-10 22:50:44 -0400

    transfered content for four pages

---
+++ pregnancyAndBirth.php
@@ -0,0 +1,77 @@
+<?php include("includes/doctype_html.php") ?>
+<?php include("includes/head_start.php") ?>
+<?php include("includes/head_end.php") ?>
+<?php include("includes/body_start.php") ?>
+<?php include("includes/globalnav.php") ?>
+
+        <!-- unique -->
+        <div id="content">
+            
+            <?php include("includes/sidebar.php") ?>
+            
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    PREGNANCY AND BIRTH
+                </h1>
+                
+                <p>
+                    <dl>
+						<dt>
+							<a href="http://www.childbirth.org" target="_blank">
+								ChildBirth.org
+							</a>						
+						</dt>
+						<dd>
+							This site has excellent references to common interventions as 
+							well as an interactive birth plan.  The motto of this site is 
+							"Birth is a natural process, not a medical procedure".
+						</dd>
+						<dt>
+							<a href="http://www.bradleybirth.com" target="_blank">
+								The Bradley Method Homepage
+							</a>
+						</dt>
+						<dd>
+							I listed this not only to introduce you to The Bradley Method, 
+							but for its link on the Pregnancy Diet. The protein counter is 
+							especially useful. 
+						</dd>
+						<dt>
+							<a href="http://www.lamaze.org" target="_blank">
+								Lamaze International Homepage
+							</a>
+						</dt>
+						<dd>
+							This site contains info on how to find a class in your area, 
+							books for expectant parents, and the basic principles of the 
+							Lamaze method of natural childbirth.
+						</dd>
+						<dt>
+							<a href="http://pregnancy.about.com" target="_blank">
+								About.com Pregnancy &amp; Childbirth
+							</a>
+						</dt>
+						<dd>
+							Very inclusive site with information on everything from conception 
+							to baby care.  I like the clip art section too!
+						</dd>
+						<dt>
+							<a href="http://www.essortment.com/pregnancy-parenting" target="_blank">
+								eSSORTMANT.com Pregnancy &amp; Parenting
+							</a>
+						</dt>
+						<dd>
+							An inclusive site with  a variety of topics related to pregnancy, 
+							birth, breastfeeding and baby care.
+						</dd>
+                    </dl>
+                </p>
+            </div>
+        </div>
+        <!-- /unique -->
+
+<?php include("includes/footer.php");?>
+<?php include("includes/body_end.php");?>
+<?php include("includes/html_end.php");?>
+
