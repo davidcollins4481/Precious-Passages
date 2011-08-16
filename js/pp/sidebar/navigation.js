5f407a3fccdd38aa998e7dfd1ebdd452ca3e5149
David Collins <davidcollins4481@gmail.com>
2011-08-15 23:33:30 -0400

    move rendering of nav to js. Incremental commit before refactor

--- js/pp/sidebar/navigation.js
+++ js/pp/sidebar/navigation.js
@@ -1,5 +1,6 @@
 dojo.provide("pp.sidebar.navigation");
 dojo.require("dijit.layout.AccordionContainer");
+dojo.require("dijit.layout.ContentPane");
 dojo.require("pp.form.asyncForm");
 dojo.require("pp.sidebar.login");
 dojo.require("pp.sidebar.userPanel");
@@ -12,9 +13,66 @@ dojo.declare(
     [dijit._Widget, dijit._Templated],
     {
         templatePath: dojo.moduleUrl("pp", "templates/navigation.html"),
-        postCreate: function(args) {
-            var container = this.parentContainerNode;
-            dojo.parser.parse(container);
+        dataSource: null,
+
+        postCreate: function() {
+            this._loadDataSource();
+        },
+
+        // this is the first place you can reliably
+        // access data for links.json
+        init: function() {
+            this._initServicesMenu();
+        },
+
+        _initServicesMenu: function() {
+            var servicesContainer = new dijit.layout.AccordionContainer({
+                class: "submenu"
+            });
+
+            dojo.place(servicesContainer.domNode, this.servicesNode, "first");
+
+            // set sub-children
+            var data = this.dataSource["services"];
+
+            for (submenu in data) {
+                var name = submenu;
+                var links = data[name];
+
+                var linkify = function(l) {
+                    return '<a href="' + l.url + '">' + l.name + '</a>';
+                };
+
+                var content = "";
+
+                dojo.forEach(links, function(link) {
+                    content += linkify(link);
+                });
+
+                servicesContainer.addChild(new dijit.layout.ContentPane({
+                    title: name,
+                    content: content
+                }));
+            }
+
+            servicesContainer.startup();
+        },
+
+        _initResourcesMenu: function() {
+
+        },
+
+        _loadDataSource: function() {
+            var self = this;
+
+            dojo.xhrGet({
+                url: "/js/pp/links.json",
+                handleAs: "json",
+                handle: function(data){
+                    self.dataSource = data;
+                    self.init();
+                }
+            });
         }
     }
 );
