cc840e7bbd8f488c6bf37acf7dcc802b002de01c
David Collins <davidcollins4481@gmail.com>
2011-08-29 18:10:39 -0400

    think I have a good handle on how to get what I need from Picasa. Still need to build out finalized data structures for frontend parsing

--- application/controllers/gallery.php
+++ application/controllers/gallery.php
@@ -3,6 +3,41 @@
 class gallery extends CI_Controller {
 
     public function index() {
+        $this->load->library('zend');
+        $this->zend->load('Zend/Gdata/Photos');
+        $this->zend->load('Zend/Gdata/ClientLogin');
+        $this->zend->load('Zend/Gdata/Photos/AlbumQuery');
+
+        $gp = new Zend_Gdata_Photos();
+
+        try {
+            $query = $gp->newAlbumQuery();
+
+            $query->setUser("davidcollins4481");
+            // album MUST be set to public for this to work!
+            // comes from URL of RSS feed for album
+
+            //$query->setAlbumId("5577722144030642753");
+            //$query->setAlbumName("Arizona");
+
+            $albumFeed = $gp->getAlbumFeed($query);
+
+            foreach ($albumFeed as $photoEntry) {
+                echo "***<br/>";
+
+                // See Gdata/App/FeedEntryParent.php
+                echo $photoEntry->title->text. "<br />";
+                $mediaContentArray = $photoEntry->getMediaGroup()->getContent();
+                $contentUrl = $mediaContentArray[0]->getUrl();
+                echo $contentUrl;
+
+                echo "<br/>***";
+            }
+        } catch (Zend_Gdata_App_Exception $e) {
+            // hmm...not sure i want to do this here
+            echo "Error: " . $e->getMessage();
+        }
+
         $this->load->view('gallery.php');
     }
 }
