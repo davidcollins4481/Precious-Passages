95000ebdd75c4facbfea47e6cd65801b5d21c4e3
David Collins <davidcollins4481@gmail.com>
2011-09-11 18:14:06 -0400

    cross-browser, z-index fixes

--- application/views/includes/globalnav.php
+++ application/views/includes/globalnav.php
@@ -5,7 +5,7 @@
         </div>
         
         <div id="header">
-            <img src="/images/seg2.png" style="position:absolute;opacity: .1;top: -189px;left: 0.8px; -webkit-transform: rotate(237deg);(-90deg); width: 700px;"/>
+            <img src="/images/seg2.png" class="leaf"/>
             <h1>Precious Passage Birth Services</h1>
             <!--<h1><img src="/images/brand-header.png" alt="Precious Passages Birth Services" /></h1>-->
             <h2>Supporting Women and Nurturing Families</h2>
