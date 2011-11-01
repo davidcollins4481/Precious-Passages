b946e6b863af18fea52052a3570cbb159c690bcc
Diana Collins <dianacollins323@gmail.com>
2011-11-01 10:45:48 -0400

    nsfered content and added pages to navigation

---
+++ application/views/static/homeBirth.php
@@ -0,0 +1,137 @@
+<?php $this->load->view("includes/doctype_html.php") ?>
+<?php $this->load->view("includes/head_start.php") ?>
+<?php $this->load->view("includes/head_end.php") ?>
+<?php $this->load->view("includes/body_start.php") ?>
+<?php $this->load->view("includes/globalnav.php") ?>
+
+        <!-- unique -->
+        <div id="content">
+            <?php $this->load->view("includes/sidebar.php") ?>
+            
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    Homebirth
+                </h1>
+                <p>
+					To date, I have had five children.  The first in the hospital, the 
+					other four at home.  After experiencing both, I really prefer 
+					homebirthing. I support women regardless of their choice of birthplace; 
+					in fact, couples birthing in a hospital need an advocate more than 
+					couples birthing at home.  Homebirth is not the right choice for every 
+					couple, but it is a viable alternative for any low risk mother.  As a 
+					DEM, I strongly suggest that any couple hoping to achieve a drug and 
+					intervention free birth consider homebirthing.  Here are a few of the 
+					advantages to having your baby at home. 
+					<ul>
+						<li>
+							Birth is perceived as a normal physiologic event, not a medical 
+							procedure or emergency.
+						</li>
+						<li>
+							Complete control over birth environment (noise level, lighting, 
+							number of people present) which leads to increased feeling of 
+							safety and ability to cope with labor.
+						</li>
+						<li>
+							Freedom to labor the way you want to.  Women feel much less 
+							inhibited in the privacy of their home.
+						</li>
+						<li>
+							Increased bonding with baby, partner and other children.
+						</li>
+						<li>
+							Formation of a relationship with midwife and doula which enhances 
+							the birth experience.
+						</li>
+						<li>
+							Reduced chance of intervention by care provider.
+						</li>
+						<li>
+							No temptation to take pain relieving drugs.
+						</li>
+						<li>
+							No interruption of labor by a trip to the hospital.
+						</li>
+						<li>
+							No routine medical procedures done to baby.
+						</li>
+						<li>
+							Strong support of breastfeeding and immediate contact between mom 
+							and baby.
+						</li>
+					</ul>
+                </p>
+                <p>
+					Here are some links to information about homebirth.
+					<ul>
+						<li>
+							<a href="http://www.changesurfer.com/Hlth/homebirth.html" target="_blank">
+								The Safety of Home Birth Studies
+							</a>		
+						</li>
+						<li>
+							<a href="http://www.traditionalmidwife.com/actmhomebirth.html" target="_blank">
+								Is Homebirth for You?
+							</a>
+						</li>
+						<li>
+							<a href="http://www.midwiferytoday.com/articles/homebirthchoice.asp" target="_blank">
+								The Homebirth Choice *
+							</a>
+						</li>
+						<li>
+							<a href="http://www.midwiferytoday.com/articles/healing.asp" target="_blank">
+								Healing Through Homebirth
+							</a>
+						</li>
+						<li>
+							<a href="http://www.midwiferytoday.com/articles/homebirthissues.asp" target="_blank">
+								Homebirth: What Are the Issues?
+							</a>
+						</li>
+						<li>
+							<a href="http://www.midwiferytoday.com/articles/normalbirth.asp" target="_blank">
+								Normal Birth: Do We Believe? Can We Remember?
+							</a>
+						</li>
+						<li>
+							<a href="http://www.midwiferytoday.com/articles/notamong.asp" target="_blank">
+								Not Among Strangers
+							</a>
+						</li>
+						<li>
+							<a href="http://nsrc.sfsu.edu/article/welcoming_baby_or_not" target="_blank">
+								Welcoming Baby, or Not?
+							</a>
+						</li>
+						<li>
+							<a href="/d/nineTips">
+								9 Tips for a Successful Home Birth
+							</a>
+						</li>
+						<li>
+							<a href="/d/isItSafe">
+								Home Birth and Out-of-Hospital Birth: Is it Safe?
+							</a>
+						</li>
+						<li>
+							<a href="/d/safeAsItGets">
+								Homebirth: As Safe As it Gets
+							</a>
+						</li>
+					</ul>
+					* If you are planning a homebirth, this is the place to begin gathering 
+					your information.  Included in this area of the Midwifery Today site is 
+					information on the safety and risks of homebirth, who assists at a 
+					homebirth and how to find a midwife.
+                </p>
+            </div>
+        </div>
+        <!-- /unique -->
+
+<?php $this->load->view("includes/footer.php");?>
+<?php $this->load->view("includes/body_end.php");?>
+<?php $this->load->view("includes/html_end.php");?>
+
+
