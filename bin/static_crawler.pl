294f43a6304dd82066fee42c04a449b8e0c19bbc
David Collins <davidcollins4481@gmail.com>
2011-11-03 08:45:29 -0400

    update crawler

--- bin/static_crawler.pl
+++ bin/static_crawler.pl
@@ -2,14 +2,12 @@
 
 use strict;
 use LWP::UserAgent;
-use HTML::Parser;
 use File::Basename;
 
 # config
 my $content_directory = '/home/david/Projects/Precious-Passages/application/views/static';
 my $domain = 'http://david.preciouspassage.com';
-# not sure if CSV is gonna work here. Check for CSV module
-my $out_file = "results.csv";
+my $out_file = "results";
 ###
 
 my $ua = LWP::UserAgent->new;
@@ -24,6 +22,7 @@ foreach my $file (glob "${content_directory}/*") {
     if ($response->is_success) {
         my $content = $response->decoded_content;
         my $parsed = parse_page($content);
+        next if !$parsed;
         $$parsed{path} = $path;
         push(@results, $parsed);
     } else {
@@ -33,6 +32,7 @@ foreach my $file (glob "${content_directory}/*") {
 
 output_results(\@results);
 
+#TODO: maybe some kind of content validation. Check if there is enough content
 sub parse_page {
     my $html = shift;
     
@@ -42,12 +42,18 @@ sub parse_page {
     $html =~ s|\n||g;
     $html =~ s|\s{2,}||g;
     $html =~ s|\t||g;
+    
     $html =~ s|<!-- exclude -->.*<!-- /exclude -->||g;
-    $html =~ m|<!-- unique -->(.*)<!-- /unique -->|g;
+    my $valid = $html =~ m|<!-- unique -->(.*)<!-- /unique -->|g;
+
+    return undef if !$valid;
 
     my $content = $1;
     $content =~ s|<.+?>| |g;
     $content =~ s|\s{2,}||g;
+    
+    return undef if !$title || !$content;
+    
     return {
         title   => $title,
         content => $content,
@@ -58,10 +64,10 @@ sub output_results {
     my $results = shift;
     
     open(OUT, ">$out_file") || die $!;
-    print OUT "title,path,content\n";
+    print OUT "text|||url|||title\n";
 
     foreach my $r (@$results) {
-        print OUT $$r{title} .',' . $$r{path} .','.$$r{content}."\n";
+        print OUT $$r{content} .'|||' . $$r{path} .'|||'.$$r{title}."\n";
     }
     
     close OUT;
