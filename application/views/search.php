67e5e0152699222a3da25ff0ba3a520617f17ec3
David Collins <davidcollins4481@gmail.com>
2011-11-05 01:51:48 -0400

    search pagination

--- application/views/search.php
+++ application/views/search.php
@@ -1,32 +1,56 @@
 <?php $this->load->view("includes/doctype_html.php") ?>
 <?php $this->load->view("includes/head_start.php") ?>
+<script type="text/javascript" src="/js/pp/sidebar/search.js"></script>
+
+<?php
+    $resultsPerPage = 5;
+    $pageCount = ceil(count($query) / ($resultsPerPage));
+    $style = ($pageCount > 1) ? 'display:block' : 'display:none';
+?>
+
+<script type="text/javascript">
+    var resultsPerPage = <?php echo $resultsPerPage ?>;
+    var pageCount = <?php echo $pageCount ?>;
+</script>
+
 <?php $this->load->view("includes/head_end.php") ?>
 <?php $this->load->view("includes/body_start.php") ?>
 <?php $this->load->view("includes/globalnav.php") ?>
-
-        <!-- unique -->
+    <div id="search-container">
         <div id="content">
             <?php $this->load->view("includes/sidebar.php") ?>
 
             <?php if (isset($query) && count($query)) { ?>
                 <p><?php echo $message; ?></p>
-                <?php foreach($query as $item) { ?>
-                    <div class="search-result">
+                <?php for($i = 0; $i < count($query);$i++) { ?>
+                    <div id="search-result-<?php echo $i + 1 ?>" class="search-result <?php if ($i > $resultsPerPage - 1) echo "hidden"; ?>">
                         <h2>
-                            <a href="<?php echo $item['url']; ?>">
-                                <?php echo $item['title']; ?>
+                            <a href="<?php echo $query[$i]['url']; ?>">
+                                <?php echo $query[$i]['title']; ?>
                             </a>
                         </h2>
                         <p>
-                            <?php echo $item['context_segment']; ?>
+                            <?php echo $query[$i]['context_segment']; ?>
                         </p>
                     </div>
                 <?php } ?>
             <?php } else { ?>
                 <p><?php echo $message; ?></p>
             <?php } ?>
+            
+            <div id="pagination-container" style="<?php echo $style; ?>">
+                <span id="previous" class="invisible previous">Previous</span>
+                <?php
+                    for ($i = 1;$i <= $pageCount; $i++) {
+                        $selectedClass = $i == 1 ? ' active' : '';
+                        echo '<span id="page-' . $i . '" class="pagination-link' . $selectedClass . '">' . $i. '</span>';
+                    }
+                ?>
+                <span id="next" class="next">Next</span>
+            </div>
+
         </div>
-        <!-- /unique -->
+    </div>
 
 <?php $this->load->view("includes/footer.php");?>
 <?php $this->load->view("includes/body_end.php");?>
---
+++ js/pp/sidebar/search.js
@@ -0,0 +1,84 @@
+dojo.addOnLoad(function() {
+    var currentPage = 1;
+    var search_container = dojo.query('.search-result')[0];
+
+    var setActivePage = function() {
+        dojo.query('.pagination-link').forEach(function(node) {
+            dojo.removeClass(node, 'active');
+        });
+
+        dojo.addClass(dojo.byId('page-' + currentPage), 'active');
+    };
+
+    var toggleNextPrevious = function() {
+        // previous links
+        if (currentPage > 1) {
+            dojo.removeClass(dojo.byId('previous'), "invisible");
+        } else {
+            dojo.addClass(dojo.byId('previous'), "invisible");
+        }
+
+        if (currentPage < pageCount) {
+            dojo.removeClass(dojo.byId('next'), 'invisible');
+        } else {
+            dojo.addClass(dojo.byId('next'), 'invisible');
+        }
+    };
+
+    var renderPage = function() {
+        dojo.query('.search-result').forEach(function(row) {
+            dojo.fadeOut({
+                node: row,
+                onEnd: function(node) {
+                    dojo.addClass(node, 'hidden');
+                }
+            }).play();
+        });
+
+        var end   = currentPage * resultsPerPage;
+        var start = end - (resultsPerPage-1);
+
+        for (var i = start; i <= end; i++) {
+            var row = dojo.byId("search-result-" + i);
+
+            if (!row) break;
+
+            dojo.fadeIn({
+                node: row,
+                onEnd: function(node) {
+                    dojo.removeClass(node, 'hidden');
+                }
+            }).play();
+        }
+    };
+    
+    if (search_container) {
+        dojo.connect(dojo.byId('previous'),"onclick", null, function(evt) {
+            console.log('previous');
+            currentPage--;
+            toggleNextPrevious();
+            setActivePage()
+            renderPage();
+        });
+
+        dojo.connect(dojo.byId('next'),"onclick", null, function(evt) {
+            console.log('next');
+            currentPage++;
+            toggleNextPrevious();
+            setActivePage();
+            renderPage();
+        });
+
+        var paginationLinks = dojo.query('.pagination-link');
+        dojo.forEach(paginationLinks, function(link) {
+            dojo.connect(link, "onclick", null, function(evt) {
+                var pageNumber = parseInt(this.id.match(/\d/)[0]);
+                console.log(pageNumber);
+                currentPage = pageNumber;
+                toggleNextPrevious();
+                renderPage();
+                setActivePage();
+            });
+        });
+    }
+});
