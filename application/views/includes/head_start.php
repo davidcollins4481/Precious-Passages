20581c552f81d2bcbf8a8a7ed5d8e801cf170929
David Collins <davidcollins4481@gmail.com>
2012-01-06 23:12:15 -0500

    mechanism for meta keywords w/defaults set new home page

--- application/views/includes/head_start.php
+++ application/views/includes/head_start.php
@@ -1,9 +1,12 @@
 <?php
     $t = isset($title) && $title ? $title : 'Precious Passage';
+    $default_keywords = "Erika Obert, Doula, labor support, Midwife, midwifery, pregnancy, childbirth, Ohio, Akron, pregnant, labor, support, homebirth";
+    $keywords = isset($meta_keywords) && $meta_keywords ? $meta_keywords : $default_keywords;
 ?>
 
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <meta name="keywords" content="<?php echo $keywords ?>">
     <title><?php echo $t ?></title>
         <?php $this->load->view("includes/css.php") ?>
         <?php $this->load->view("includes/js.php") ?>
