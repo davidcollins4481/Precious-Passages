f3dc655bcc746bf1769e06290edaf81e643f52fc
Diana Collins <dianacollins323@gmail.com>
2012-01-06 23:14:05 -0500

    created index

---
+++ application/views/static/index.php
@@ -0,0 +1,28 @@
+<?php $this->load->view("includes/doctype_html.php") ?>
+<?php $this->load->view("includes/head_start.php", array("title" => "")) ?>
+<?php $this->load->view("includes/head_end.php") ?>
+<?php $this->load->view("includes/body_start.php") ?>
+<?php $this->load->view("includes/globalnav.php") ?>
+
+        <!-- unique -->
+        <div id="content">
+            
+            <!-- exclude --><?php $this->load->view("includes/sidebar.php") ?><!-- /exclude -->
+            
+            <div id="mainbar">
+                <h1>
+                    <a name="intro" id="intro"></a>
+                    Supporting women, nurturing families.
+                </h1>
+                
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+        <!-- /unique -->
+
+<?php $this->load->view("includes/footer.php");?>
+<?php $this->load->view("includes/body_end.php");?>
+<?php $this->load->view("includes/html_end.php");?>
+
