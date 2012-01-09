1466d2233a40793ed68e5c6119d02df03fa4e71a
David Collins <davidcollins4481@gmail.com>
2012-01-08 19:12:40 -0500

    add some static content for sample birth plan 1

--- application/views/birthplans/plan1.php
+++ application/views/birthplans/plan1.php
@@ -3,24 +3,57 @@
 <?php $this->load->view("includes/head_end.php") ?>
 <?php $this->load->view("includes/body_start.php") ?>
 <?php $this->load->view("includes/globalnav.php") ?>
-
         <!-- unique -->
         <div id="content">
-            
             <!-- exclude --><?php $this->load->view("includes/sidebar.php") ?><!-- /exclude -->
-            
             <div id="mainbar">
                 <h1>
                     <a name="intro" id="intro"></a>
                     Sample Plan 1
                 </h1>
-                
-                
+                <p>
+                    <span style="font-family: Arial; color: windowtext">This Birth Plan comes from </span>
+                    <span style="font-family: Arial">
+                        <a style="color: #324094; font-weight: normal; font-style: normal; text-decoration: none; text-underline: none; text-line-through: none" href="http://sheknows.com/inside/look/241.htm">
+                            http://sheknows.com/inside/look/241.htm
+                        </a>
+                    </span>
+                    <b><span style="font-family: Arial; color: windowtext">.&nbsp;</span></b>
+                    <span style="font-family: Arial; color: windowtext">This is just one of several online interactive birth plans.&nbsp; </span></p>
+                    <p><b><span style="font-family: Arial; color: windowtext">Birth plan creator</span></b><span style="font-family: Arial; color: windowtext">
+                    </span></p>
+                    <p><b><span style="font-family: Arial; color: windowtext">Summarizing your birth
+                    preferences is easy with our interactive checklist! Simply fill in the blanks at
+                    the top of the plan below, then check the items you would like to see printed on
+                    your birth plan.&nbsp; Feel free to add any additional items you deem important.</span></b></p>
+                    <p>&nbsp;</p>
+                    <p><b><span style="font-family: Arial; color: windowtext">A few important notes</span></b><span style="font-family: Arial; color: windowtext"><br>
+                    The reason for creating a birth plan is so that everyone understands your hopes
+                    and desires for labor and birth. It is a great place to begin discussions with
+                    your caregiver(s). This is <i>not</i> a contract, but a statement of your
+                    preferences. (<a style="color: #324094; font-weight: normal; font-style: normal; text-decoration: none; text-underline: none; text-line-through: none" href="http://PregnancyAndBaby.com/inside/look/26.htm"><span style="color: windowtext">Click
+                    here</span></a> for a great article about birth plans.) </span></p>
+                    <p><span style="font-family: Arial; color: windowtext">Birth plans are best kept
+                    short and to the point -- lots of details may be lost on medical support staff.
+                    You may wish to create two plans: one for you and your support team (coach,
+                    doula, etc.) and another, more concise document (about a page long) for your
+                    caregiver and the hospital/birth center staff. </span></p>
+                    <p><span style="font-family: Arial; color: windowtext">It is <i>very important</i>
+                    that you talk about the procedures and/or choices that appear your plan with
+                    your caregiver(s). Not only do obstetric practices often vary by caregiver,
+                    hospital, state and country, there are often important factors involved. It is
+                    your responsibility to evaluate and understand each choice you make. </span></p>
+                    <p><b><span style="font-family: Arial; color: windowtext">Instructions:</span></b><span style="font-family: Arial; color: windowtext">
+                    On the plan below, all section headings are checked by default. If you do not
+                    select any of the options underneath a particular heading (and keep the blank
+                    boxes clear), uncheck the heading box in order to avoid having a spare heading
+                    with no additional text. </span></p>
+                    <p><span style="font-family: Arial; color: windowtext">When you're done, press
+                    the &quot;create&quot; button at the end of the page! Save the finished plan to your hard
+                    drive as a .htm or .html file or print it out. </span></p>
             </div>
         </div>
         <!-- /unique -->
-
 <?php $this->load->view("includes/footer.php");?>
 <?php $this->load->view("includes/body_end.php");?>
 <?php $this->load->view("includes/html_end.php");?>
-
