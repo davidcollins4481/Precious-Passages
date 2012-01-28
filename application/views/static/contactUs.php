233cf106f45eaa02969b462ae270a810fd72ea9f
Diana Collins <dianacollins323@gmail.com>
2012-01-28 15:33:21 -0500

    changes to contact page and images directory

--- application/views/static/contactUs.php
+++ application/views/static/contactUs.php
@@ -15,55 +15,117 @@
                     Contact Us
                 </h1>
                 
+                <table>
+                    <tr>
+                        <td class="contactTbl">
+                            <h3>
+                                Midwifery &amp; Doula Services
+                            </h3>
+                            <a href="mailto:erikao@preciouspassage.com">
+                                Erika Obert, DEM
+                            </a>
+                            <br/>
+                            (330) 577-5180
+                            <br/>
+                            <br/>
+                            <a href="mailto:">
+                                Alyssa Taylor, DEM
+                            </a>
+                        </td>
+                        <td class="contactTbl">
+                            <h3>
+                                Placental Encapsulation
+                            </h3>
+                            <a href="mailto:erikao@preciouspassage.com">
+                                Erika Obert, DEM
+                            </a>
+                            <br/>
+                            (330) 577-5180
+                            <br/>
+                            <br/>
+                            <a href="mailto:">
+                                Alyssa Taylor, DEM
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="contactTbl">
+                            <h3>
+                                Belly Casting
+                            </h3>
+                            <!--<a href="mailto:">
+                                
+                            </a>-->
+                        </td>
+                        <td class="contactTbl">
+                            <h3>
+                                Photography
+                            </h3>
+                            <!--<a href="">
+                                
+                            </a>-->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="contactTbl">
+                            
+                        </td>
+                        <td class="contactTbl">
+                            
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="contactTbl">
+                            
+                        </td>
+                        <td class="contactTbl">
+                            
+                        </td>
+                    </tr>
+                </table>
+                
+                <hr/>
+                
+                <table>
+                    <tr>
+                        <td class="contactTbl">
+                            <h3>
+                                Office Address
+                            </h3>
+                            **Some Address in Akron**
+                            <br/>
+                            **map link**
+                            
+                        </td>
+                        <td class="contactTbl">
+                            <h3>
+                                Mailing Address
+                            </h3>
+                            **the address you would like to recieve mail at if different from office 
+                            address**
+                        </td>
+                    </tr>
+                </table>
+                
+                <hr/>
+                
                 <h3>
-                    Midwifery &amp; Doula Services
+                    Office Hours
                 </h3>
                 <p>
-                    <a href="mailto:erikao@preciouspassage.com">
-                        Erika Obert, DEM
-                    </a>
+                    Our office hours are by appointment only. Appointments may be scheduled
+                    during the following hours:
                     <br/>
-                    (330) 577-5180
-                </p>
-                <p>
-                    <a href="mailto:">
-                        Alyssa Taylor, DEM
-                    </a>
+                    **Your office hours**
                 </p>
                 
-                <h3>
-                    Placental Encapsulation
-                </h3>
-                <p>
-                    <a href="mailto:erikao@preciouspassage.com">
-                        Erika Obert, DEM
-                    </a>
-                    <br/>
-                    (330) 577-5180
-                </p>
-                <p>
-                    <a href="mailto:">
-                        Alyssa Taylor, DEM
-                    </a>
-                </p>
+                <hr/>
                 
                 <h3>
-                    Belly Casting
+                    Map to Our Office
                 </h3>
-                <p>
-                    <a href="">
-                        
-                    </a>
-                </p>
                 
-                <h3>
-                    Photography
-                </h3>
-                <p>
-                    <a href="">
-                        
-                    </a>
-                </p>
+                <iframe width="220" height="220" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=345+Poplar+Ave+NW,+Canton,+OH+44708&amp;aq=0&amp;oq=345+Popla&amp;sll=37.0625,-95.677068&amp;sspn=37.735377,56.513672&amp;ie=UTF8&amp;hq=&amp;hnear=345+Poplar+Ave+NW,+Canton,+Ohio+44708&amp;t=m&amp;ll=40.802635,-81.420965&amp;spn=0.019491,0.025749&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=345+Poplar+Ave+NW,+Canton,+OH+44708&amp;aq=0&amp;oq=345+Popla&amp;sll=37.0625,-95.677068&amp;sspn=37.735377,56.513672&amp;ie=UTF8&amp;hq=&amp;hnear=345+Poplar+Ave+NW,+Canton,+Ohio+44708&amp;t=m&amp;ll=40.802635,-81.420965&amp;spn=0.019491,0.025749&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left" target="_blank">View Larger Map</a></small>
             </div>
         </div>
         <!-- /unique -->
