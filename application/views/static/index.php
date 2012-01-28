95631e978f4bdfdd3144d779f531cd46012fbf02
Diana Collins <dianacollins323@gmail.com>
2012-01-28 10:25:59 -0500

    created index page and added css for home page specific elements:wq

--- application/views/static/index.php
+++ application/views/static/index.php
@@ -21,69 +21,84 @@
             <div id="mainbar">
                 <h1>
                     <a name="intro" id="intro"></a>
-                    Supporting women, nurturing families,
+                    "Supporting women, nurturing families"
                 </h1>
                 
-                <img src="/images/resources/erika2.jpg" alt="Erika" title="Erika" class="homeLeft" />
-                
-                <img src="/images/resources/alyssa.jpg" alt="Alyssa" title="Alyssa" class="homeRight" />
-                
-                <h1>
-                    and promoting family centered births.
-                </h1>
-                
-                <p>
-                    
+                <p class="homep">
+                    Erika Obert founded Precious Passage Birth Services in 2004 to serve 
+                    birthing families in the Northeast Ohio area as a doula.  Since that 
+                    time, the business has expanded to offer home birth midwifery as well 
+                    as several other services.  Every member of Precious Passage Birth 
+                    Services' staff believes that birth can be a transformative event in a 
+                    woman's life.  It is our heartfelt desire to give you the gift of a birth 
+                    experience that you will want to remember.
                 </p>
-                <p>
-                    To date, I have had six children.&nbsp; 
-                    The first in the hospital, the other five at home. After experiencing 
-                    both, I really prefer homebirthing. I support women regardless of their 
-                    choice of birthplace; in fact, couples birthing in a hospital need an 
-                    advocate more than couples birthing at home. Homebirth is not the right 
-                    choice for every couple, but it is a viable alternative for any low risk 
-                    mother. As a DEM, I strongly suggest that any couple hoping to achieve 
-                    a drug and intervention free birth consider homebirthing. Here are a 
-                    few of the advantages to having your baby at home.
-                    <ul>
-                        <li>
-                            Birth is perceived as a normal physiologic event, not a medical
-                            procedure or emergency.
-                        </li>
-                        <li>
-                            Complete control over birth environment (noise level, lighting,
-                            number of people present) which leads to increased feeling of 
-                            safety and ability to cope with labor.
-                        </li>
-                        <li>
-                            Freedom to labor the way you want to.&nbsp; Women feel much 
-                            less inhibited in the privacy of their home.
-                        </li>
-                        <li>
-                            Increased bonding with baby, partner and other children.
-                        </li>
-                        <li>
-                            Formation of a relationship with midwife and doula which 
-                            enhances the birth experience.
-                        </li>
-                        <li>
-                            Reduced chance of intervention by care provider.
-                        </li>
-                        <li>
-                            No temptation to take pain relieving drugs.
-                        </li>
-                        <li>
-                            No interruption of labor by a trip to the hospital.
-                        </li>
-                        <li>
-                            No routine medical procedures done to baby.
-                        </li>
-                        <li>
-                            Strong support of breastfeeding and immediate contact 
-                            between mom and baby. 
-                        </li>
-                    </ul>
-                </p>
+                
+                <img src="/images/resources/erika.jpg" alt="Erika" title="Erika" class="homeLimg" />
+                
+                <img src="/images/resources/alyssa2.jpg" alt="Erika" title="Erika" class="homeRimg" />
+                
+                <h2>
+                    Precious Passage offers these services:
+                </h2>
+                
+                <ul>
+                    <li>
+                        Homebirth Midwifery
+                    </li>
+                    <li>
+                        Doula
+                    </li>
+                    <li>
+                        Placental Encapsulation
+                    </li>
+                    <li>
+                        Belly Casting
+                    </li>
+                    <li>
+                        Pregnancy, Birth, and Newborn Photography
+                    </li>
+                </ul>
+                
+                <br/>
+                
+                <h3>
+                    Precious Passage Birth Services proudly endorses:
+                <br/>
+                    The Michigan School of Traditional Midwifery
+                </h3>
+                
+                <a href="http://www.traditionalmidwife.com/" target="_blank">
+                    <img src="/images/resources/MSTM.png" alt="MSTM" title="MSTM" class="homeMSTMimg" />
+                </a>
+                
+                <br/>
+                
+                <h4>
+                    It is not the mountains ahead that wear you down, it is the grain of sand
+                    in your shoe.
+                </h4>
+                
+                <br/>
+                
+                <blockquote>
+                    I believe that imagination is stronger than knowledge.
+                    That myth is more potent than history.
+                    That dreams are more powerful than facts.
+                    That hope always triumphs over experience.
+                    That laughter is the only cure for grief.
+                    And I believe that love is stronger than death.
+                    <br/>
+                    - Robert Fulghum 2003
+                </blockquote>
+                
+                <br/>
+                
+                <h5>
+                    *Note: The opinions found on this site are my own and should not be 
+                    construed as endorsing or promoting any organization, person, or method.
+                </h5>
+                
             </div>
         </div>
         <!-- /unique -->
