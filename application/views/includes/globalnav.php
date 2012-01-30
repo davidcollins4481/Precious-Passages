b7bb06354d9f18e1463f2b1f6b036405f196963a
David Collins <davidcollins4481@gmail.com>
2012-01-29 21:10:34 -0500

    add link to home page in top global nav (issue #27)

--- application/views/includes/globalnav.php
+++ application/views/includes/globalnav.php
@@ -4,9 +4,9 @@
                     <img class="main-header-logo" src="/images/logo5.png" />
                 </div>
                 <div class="link-container">
-                    <!--<a href="/">home</a>-->
+                    <a href="/">home</a>
                     <a href="/gallery">gallery</a>
-                    <a href="/d/aboutUs">about us</a>
+                    <a href="/d/aboutUs">about</a>
                     <a href="/blog">blog</a>
                     <a href="/d/testimonials">testimonials</a>
                     <a href="/d/contactUs">Contact Us</a>
